98887882c127db684b8a9795e262bca4c0d01fae
Shafiq Daniel <shafdanny@gmail.com>
2016-01-28 21:39:57 +0100

    Initial commit. Setup Application and Controller Spring. Example Hello World.

---
+++ src/main/java/spring/Application.java
@@ -0,0 +1,15 @@
+package spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by shafiq on 28/01/16.
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
