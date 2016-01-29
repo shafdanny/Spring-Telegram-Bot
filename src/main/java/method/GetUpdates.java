fe9bf8fc7ac309fbc66a5b4ea388b752d64ecd67
Shafiq Daniel <shafdanny@gmail.com>
2016-01-29 04:06:32 +0100

    Preliminary method to organise updates
    For now, we use an infinite loop with a sleep timer to do the long polling. However, all the method that we have for now is being run by their own main.

--- src/main/java/method/GetUpdates.java
+++ src/main/java/method/GetUpdates.java
@@ -13,8 +13,10 @@ import object.Update;
 import object.User;
 
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,14 +26,32 @@ import java.util.List;
 public class GetUpdates {
     public static void main(String[] args){
         String url = "https://api.telegram.org/bot187744061:AAEuMsPg9J3TElLGam2bvHx8YTat344maqU/getUpdates";
+        int updateIdStart = 883762352;
 
-        RestTemplate restTemplate = new RestTemplate();
-        GetUpdatesResponse test = restTemplate.getForObject(url, GetUpdatesResponse.class);
 
-        ArrayList<Update> updateList = (ArrayList<Update>) test.getResult();
+        while(true) {
+            URI targetUrl = UriComponentsBuilder.fromUriString(url)
+                    .queryParam("offset", updateIdStart + 1)
+                    .build()
+                    .toUri();
 
-        for(Update update:updateList){
-            System.out.println(update.getMessage().getText());
+            RestTemplate restTemplate = new RestTemplate();
+            GetUpdatesResponse test = restTemplate.getForObject(targetUrl, GetUpdatesResponse.class);
+
+            ArrayList<Update> updateList = (ArrayList<Update>) test.getResult();
+
+            for (Update update : updateList) {
+                System.out.println(update.getMessage().getText());
+
+                if(update.getUpdate_id()>updateIdStart)
+                    updateIdStart = update.getUpdate_id();
+            }
+
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
 
     }
